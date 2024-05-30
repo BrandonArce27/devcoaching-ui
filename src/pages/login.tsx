afe61917ff64a644102f08d931637a383a885512
BrandonArce27 <brandonarce2712@gmail.com>
2024-05-30 15:35:17 -0600

    style(login): adding styles

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,6 +6,17 @@ import React from "react";
 
 import { useRouter } from "next/router";
 
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Flex,
+  useColorMode,
+  Heading,
+} from "@chakra-ui/react";
+
 const LOGIN_MUTATION = gql`
   mutation Login($email: String!, $password: String!) {
     login(data: { email: $email, password: $password }) {
@@ -31,6 +42,8 @@ type LoginData = {
 };
 
 function Login() {
+  const { colorMode } = useColorMode();
+  const isDark = colorMode === "dark";
   const [login, { data }] = useMutation<LoginData>(LOGIN_MUTATION);
   const router = useRouter();
 
@@ -46,7 +59,6 @@ function Login() {
     const response = await login({ variables: { email, password } });
     const token = response.data?.login.access_token;
 
-    // Store the token in local storage
     if (token) {
       localStorage.setItem("authToken", token);
       console.log("authToken", token);
@@ -55,11 +67,53 @@ function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input name="email" type="email" placeholder="Email" />
-      <input name="password" type="password" placeholder="Password" />
-      <button type="submit">Log in</button>
-    </form>
+    <>
+      <Heading
+        as="h1"
+        size="lg"
+        position="absolute"
+        top={5}
+        left={5}
+        fontFamily="Arial, sans-serif"
+      >
+        <Box as="span" color="white">
+          Dev
+        </Box>
+        <Box as="span" color="#38B2AC">
+          L
+        </Box>
+        <Box as="span" color="white">
+          ive
+        </Box>
+      </Heading>
+      <Flex minHeight="100vh" alignItems="center" justifyContent="center">
+        <Box
+          as="form"
+          onSubmit={handleSubmit}
+          p={5}
+          shadow="md"
+          borderWidth={isDark ? 0 : 1}
+        >
+          <FormControl id="email" isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input name="email" type="email" placeholder="Email" />
+          </FormControl>
+          <FormControl id="password" isRequired mt={6}>
+            <FormLabel>Password</FormLabel>
+            <Input name="password" type="password" placeholder="Password" />
+          </FormControl>
+          <Button
+            colorScheme="teal"
+            variant="outline"
+            type="submit"
+            width="full"
+            mt={4}
+          >
+            Log in
+          </Button>
+        </Box>
+      </Flex>
+    </>
   );
 }
 
