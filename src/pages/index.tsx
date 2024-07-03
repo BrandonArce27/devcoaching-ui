8ce41c503c406c4537329095c40d274c7defecfe
BrandonArce27 <brandonarce2712@gmail.com>
2024-07-02 23:54:17 -0600

    refactor(api): remove duplicate Apollo code and bearer token handling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,102 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 import { useRouter } from "next/router";
 
-import { ChakraProvider, Button } from "@chakra-ui/react";
+import { Layout } from "@/shared/components";
 
-import { useQuery, gql } from "@apollo/client";
+import { useUserQuery } from "@/shared/generated";
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-
-import { setContext } from "@apollo/client/link/context";
-
-import Layout from "../../components/molecules/layout";
-
-const GET_USER = gql`
-  query user {
-    user {
-      id
-      uuid
-      email
-      firstName
-      lastName
-      createdAt
-      updatedAt
-    }
-  }
-`;
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:8080/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("authToken");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-interface User {
-  id: string;
-  uuid: string;
-  email: string;
-  firstName: string;
-  lastName: string;
-  createdAt: string;
-  updatedAt: string;
-}
+import { StorageService } from "@/shared/services";
 
 export default function Home() {
   const router = useRouter();
 
-  const { loading, error, data } = useQuery(GET_USER);
-
-  const [user, setUser] = useState<User | null>(null);
+  const { loading, error, data: { user } = {} } = useUserQuery();
 
   useEffect(() => {
-    const token = localStorage.getItem("authToken");
+    const token = StorageService.getAuthToken();
 
-    if (!token) {
-      router.push("/login");
-    } else if (data) {
-      setUser(data.user);
-    }
-  }, [data, router]);
+    if (!token) router.push("/login");
+  }, [router]);
 
   if (loading) return "Loading...";
+
   if (error) return `Error! ${error.message}`;
 
-  console.log(data.user);
-
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider>
-        <Layout>
-          <main>
-            {user && (
-              <div>
-                <h2>
-                  Hola: {user.firstName} {user.lastName}
-                </h2>
-              </div>
-            )}
-          </main>
-        </Layout>
-      </ChakraProvider>
-    </ApolloProvider>
+    <Layout>
+      {user && (
+        <div>
+          <h2>
+            Hola: {user.firstName} {user.lastName}
+          </h2>
+        </div>
+      )}
+    </Layout>
   );
 }
