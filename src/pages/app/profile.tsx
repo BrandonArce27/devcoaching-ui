7b79a72a90a06fdaf0014c1cc79be90b417f0ae9
BrandonArce27 <brandonarce2712@gmail.com>
2024-07-03 00:04:54 -0600

    feat(profile): create profile module component

---
+++ src/pages/app/profile.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "@chakra-ui/react";
+
+import React from "react";
+
+import { Layout } from "@/shared/components";
+
+function profile() {
+  return (
+    <Layout>
+      <Heading>User Profile</Heading>
+    </Layout>
+  );
+}
+
+export default profile;
