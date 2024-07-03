0cbfcd92e83e1740b7e0ecb050f0c22e515cc59a
BrandonArce27 <brandonarce2712@gmail.com>
2024-07-02 23:38:27 -0600

    feat(exports): add index.ts for exports

---
+++ src/shared/models/index.ts
@@ -0,0 +1,5 @@
+import User from "./user.model";
+
+export { User };
+
+export * from "./user-profile.interface";
---
+++ src/shared/types/index.ts
@@ -0,0 +1,4 @@
+export * from "./paths.type";
+export * from "./translate.type";
+export * from "./global-states.type";
+export * from "./user.type"
