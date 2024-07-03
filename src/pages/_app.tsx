a6ce2a9961ed87a925f6ae187b93026113ca8443
BrandonArce27 <brandonarce2712@gmail.com>
2024-07-02 23:40:47 -0600

    refactor(_app.tsx): wrap app component with provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,51 +1,17 @@
-import "@/styles/globals.css";
+import { FC } from "react";
 
 import type { AppProps } from "next/app";
 
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Provider } from "@/shared/config/provider";
 
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
+import "@/styles/globals.css";
 
-import { setContext } from "@apollo/client/link/context";
-
-export default function App({ Component, pageProps }: AppProps) {
-  
-  const httpLink = createHttpLink({
-    uri: "http://localhost:8080/graphql",
-  });
-
-  const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem("authToken");
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
-  const theme = extendTheme({
-    config: {
-      initialColorMode: "dark",
-      useSystemColorMode: false,
-    },
-  });
-
+const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider theme={theme}>
-      <ApolloProvider client={client}>
-        <Component {...pageProps} />
-      </ApolloProvider>
-    </ChakraProvider>
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
   );
-}
+};
+
+export default App;
