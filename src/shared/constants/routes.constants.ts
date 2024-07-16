9f1ab0f272a8085b2827b5c0a398c13772a0a601
BrandonArce27 <brandonarce2712@gmail.com>
2024-07-16 17:12:38 -0600

    feat(navigation): implement useNavigation hook and define route types and enums

---
+++ src/shared/constants/routes.constants.ts
@@ -0,0 +1,23 @@
+export enum GlobalRoute {
+  AUTH = "auth",
+  APP = "app",
+  MODAL = "modal",
+  NOT_FOUND = "notFound",
+}
+
+export enum AppScreen {
+  ROOT = "root",
+  HOME = "home",
+  PROFILE = "profile",
+  REWARD = "reward",
+}
+
+export enum RootScreen {
+  HOME = "home",
+  PROFILE = "profile",
+}
+
+export enum AuthScreen {
+  LAUNCH = "launch",
+  LOGIN = "login",
+}
